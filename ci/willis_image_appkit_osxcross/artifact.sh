735b7028f82398baadbe95189d1113d56db4583e
nullgemm <nullgemm@mailbox.org>
2023-06-03 10:13:41 +0200

    fix artifact copy path

--- ci/willis_image_appkit_osxcross/artifact.sh
+++ ci/willis_image_appkit_osxcross/artifact.sh
@@ -3,4 +3,4 @@
 tag=$(git tag --sort v:refname | tail -n 1)
 release=willis_bin_"$tag"
 
-docker cp willis_container_appkit_osxcross:/willis/"$release" .
+docker cp willis_container_appkit_osxcross:/scripts/willis/"$release" .

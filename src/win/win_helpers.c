fa5d59c5abc5a57b735e16c61460f0ea272e5ce6
nullgemm <nullgemm@mailbox.org>
2023-08-06 12:04:41 +0200

    add win helpers

--- src/win/win_helpers.c
+++ src/win/win_helpers.c
@@ -2,310 +2,124 @@
 #include "common/willis_private.h"
 #include "win/win.h"
 #include "win/win_helpers.h"
-#include "nix/nix.h"
 
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 
-// HACK
-// we will use pointer aliasing with this custom structure
-// to add the missing `mask` field to `xcb_input_event_mask_t`
-// instead of using the misleading (but common) packing hack thing
-struct willis_xinput_event_mask
+// sparse LUT for keycode translation
+static const enum willis_event_code keycode_table_win[256] =
 {
-	xcb_input_device_id_t deviceid;
-	uint16_t mask_len;
-	// lol who needs padding
-	uint32_t mask;
+[VK_BACK        ] = WILLIS_KEY_BACKSPACE,
+[VK_TAB         ] = WILLIS_KEY_TAB,
+[VK_RETURN      ] = WILLIS_KEY_ENTER,
+[VK_SHIFT       ] = WILLIS_KEY_SHIFT_LEFT,
+[VK_CONTROL     ] = WILLIS_KEY_CTRL_LEFT,
+[VK_MENU        ] = WILLIS_KEY_ALT_LEFT,
+#if 0
+[VK_PAUSE       ] = WILLIS_KEY_PAUSE,
+#endif
+[VK_CAPITAL     ] = WILLIS_KEY_CAPS_LOCK,
+[VK_ESCAPE      ] = WILLIS_KEY_ESCAPE,
+[VK_SPACE       ] = WILLIS_KEY_SPACE,
+[VK_PRIOR       ] = WILLIS_KEY_PAGE_UP,
+[VK_NEXT        ] = WILLIS_KEY_PAGE_DOWN,
+[VK_END         ] = WILLIS_KEY_END,
+[VK_HOME        ] = WILLIS_KEY_HOME,
+[VK_LEFT        ] = WILLIS_KEY_LEFT,
+[VK_UP          ] = WILLIS_KEY_UP,
+[VK_RIGHT       ] = WILLIS_KEY_RIGHT,
+[VK_DOWN        ] = WILLIS_KEY_DOWN,
+[VK_SNAPSHOT    ] = WILLIS_KEY_PRINT_SCREEN,
+[VK_INSERT      ] = WILLIS_KEY_INSERT,
+[VK_DELETE      ] = WILLIS_KEY_DELETE,
+['0'            ] = WILLIS_KEY_0,
+					WILLIS_KEY_1,
+					WILLIS_KEY_2,
+					WILLIS_KEY_3,
+					WILLIS_KEY_4,
+					WILLIS_KEY_5,
+					WILLIS_KEY_6,
+					WILLIS_KEY_7,
+					WILLIS_KEY_8,
+					WILLIS_KEY_9,
+['A'            ] = WILLIS_KEY_A,
+					WILLIS_KEY_B,
+					WILLIS_KEY_C,
+					WILLIS_KEY_D,
+					WILLIS_KEY_E,
+					WILLIS_KEY_F,
+					WILLIS_KEY_G,
+					WILLIS_KEY_H,
+					WILLIS_KEY_I,
+					WILLIS_KEY_J,
+					WILLIS_KEY_K,
+					WILLIS_KEY_L,
+					WILLIS_KEY_M,
+					WILLIS_KEY_N,
+					WILLIS_KEY_O,
+					WILLIS_KEY_P,
+					WILLIS_KEY_Q,
+					WILLIS_KEY_R,
+					WILLIS_KEY_S,
+					WILLIS_KEY_T,
+					WILLIS_KEY_U,
+					WILLIS_KEY_V,
+					WILLIS_KEY_W,
+					WILLIS_KEY_X,
+					WILLIS_KEY_Y,
+					WILLIS_KEY_Z,
+[VK_LWIN        ] = WILLIS_KEY_MOD_LEFT,
+[VK_RWIN        ] = WILLIS_KEY_MOD_RIGHT,
+[VK_APPS        ] = WILLIS_KEY_MENU,
+[VK_NUMPAD0     ] = WILLIS_KEY_NUM_0,
+[VK_NUMPAD1     ] = WILLIS_KEY_NUM_1,
+[VK_NUMPAD2     ] = WILLIS_KEY_NUM_2,
+[VK_NUMPAD3     ] = WILLIS_KEY_NUM_3,
+[VK_NUMPAD4     ] = WILLIS_KEY_NUM_4,
+[VK_NUMPAD5     ] = WILLIS_KEY_NUM_5,
+[VK_NUMPAD6     ] = WILLIS_KEY_NUM_6,
+[VK_NUMPAD7     ] = WILLIS_KEY_NUM_7,
+[VK_NUMPAD8     ] = WILLIS_KEY_NUM_8,
+[VK_NUMPAD9     ] = WILLIS_KEY_NUM_9,
+[VK_MULTIPLY    ] = WILLIS_KEY_NUM_ASTERISK,
+[VK_ADD         ] = WILLIS_KEY_NUM_PLUS,
+[VK_SUBTRACT    ] = WILLIS_KEY_NUM_HYPHEN_MINUS,
+[VK_DIVIDE      ] = WILLIS_KEY_NUM_SLASH,
+[VK_F1          ] = WILLIS_KEY_F1,
+[VK_F2          ] = WILLIS_KEY_F2,
+[VK_F3          ] = WILLIS_KEY_F3,
+[VK_F4          ] = WILLIS_KEY_F4,
+[VK_F5          ] = WILLIS_KEY_F5,
+[VK_F6          ] = WILLIS_KEY_F6,
+[VK_F7          ] = WILLIS_KEY_F7,
+[VK_F8          ] = WILLIS_KEY_F8,
+[VK_F9          ] = WILLIS_KEY_F9,
+[VK_F10         ] = WILLIS_KEY_F10,
+[VK_F11         ] = WILLIS_KEY_F11,
+[VK_F12         ] = WILLIS_KEY_F12,
+[VK_NUMLOCK     ] = WILLIS_KEY_NUM_LOCK,
+#if 0
+[VK_SCROLL      ] = WILLIS_KEY_SCROLL_LOCK,
+#endif
+[VK_OEM_1       ] = WILLIS_KEY_SEMICOLON,
+[VK_OEM_PLUS    ] = WILLIS_KEY_EQUALS,
+[VK_OEM_COMMA   ] = WILLIS_KEY_COMMA,
+[VK_OEM_MINUS   ] = WILLIS_KEY_HYPHEN_MINUS,
+[VK_OEM_PERIOD  ] = WILLIS_KEY_PERIOD,
+[VK_OEM_2       ] = WILLIS_KEY_SLASH,
+[VK_OEM_3       ] = WILLIS_KEY_GRAVE,
+[VK_OEM_4       ] = WILLIS_KEY_BRACKET_LEFT,
+[VK_OEM_5       ] = WILLIS_KEY_ANTISLASH,
+[VK_OEM_6       ] = WILLIS_KEY_BRACKET_RIGHT,
+[VK_OEM_7       ] = WILLIS_KEY_APOSTROPHE,
+[VK_OEM_102     ] = WILLIS_KEY_ALT_RIGHT,
+[VK_DECIMAL     ] = WILLIS_KEY_NUM_DELETE,
 };
 
-// HACK
-// universal event structure compatible with all xkb events
-// this is actually used in the code example provided with xkb
-// it is a hack used to process xkb configuration changes quickly
-struct willis_universal_xkb_event
+enum willis_event_code win_helpers_keycode_table(
+	uint8_t code)
 {
-	uint8_t response_type;
-	uint8_t xkb_type;
-	uint16_t sequence;
-	xcb_timestamp_t time;
-	uint8_t device_id;
-};
-
-union willis_magic_xkb_event
-{
-	struct willis_universal_xkb_event magic;
-	xcb_xkb_new_keyboard_notify_event_t keyboard;
-	xcb_xkb_state_notify_event_t state;
-	xcb_xkb_map_notify_event_t map;
-};
-
-
-void win_helpers_select_events_cursor(
-	struct willis* context,
-	uint32_t mask,
-	struct willis_error_info* error)
-{
-	struct win_backend* backend = context->backend_data;
-	xcb_generic_error_t* error_xcb = NULL;
-
-	// get the xinput device id for the pointer
-	xcb_input_xi_get_client_pointer_cookie_t cookie_pointer =
-		xcb_input_xi_get_client_pointer(
-			backend->conn,
-			backend->window);
-
-	xcb_input_xi_get_client_pointer_reply_t* reply_pointer =
-		xcb_input_xi_get_client_pointer_reply(
-			backend->conn,
-			cookie_pointer,
-			&error_xcb);
-
-	if (error_xcb != NULL)
-	{
-		willis_error_throw(context, error, WILLIS_ERROR_WIN_XINPUT_GET_POINTER);
-		return;
-	}
-
-	xcb_input_device_id_t dev = reply_pointer->deviceid;
-	free(reply_pointer);
-
-	// register event
-	struct willis_xinput_event_mask mask_grab =
-	{
-		.deviceid = dev,
-		.mask_len = 1,
-		.mask = mask,
-	};
-
-	xcb_void_cookie_t cookie_select =
-		xcb_input_xi_select_events(
-			backend->conn,
-			backend->root,
-			1,
-			(xcb_input_event_mask_t*) &mask_grab);
-
-	error_xcb =
-		xcb_request_check(
-			backend->conn,
-			cookie_select);
-
-	if (error_xcb != NULL)
-	{
-		willis_error_throw(context, error, WILLIS_ERROR_WIN_XINPUT_SELECT_EVENTS);
-		return;
-	}
-
-	// update internal grabbed info
-	if (mask == 0)
-	{
-		backend->mouse_grabbed = false;
-	}
-	else
-	{
-		backend->mouse_grabbed = true;
-	}
-
-	willis_error_ok(error);
-}
-
-void win_helpers_select_events_keyboard(
-	struct willis* context,
-	struct willis_error_info* error)
-{
-	struct win_backend* backend = context->backend_data;
-
-	// win variable naming madness
-	backend->xkb_select_events_details.affectNewKeyboard =
-		XCB_XKB_NKN_DETAIL_KEYCODES;
-
-	backend->xkb_select_events_details.affectState =
-		XCB_XKB_STATE_PART_MODIFIER_BASE
-		| XCB_XKB_STATE_PART_MODIFIER_LATCH
-		| XCB_XKB_STATE_PART_MODIFIER_LOCK
-		| XCB_XKB_STATE_PART_GROUP_BASE
-		| XCB_XKB_STATE_PART_GROUP_LATCH
-		| XCB_XKB_STATE_PART_GROUP_LOCK;
-
-	// this is only executed once, let's just copy
-	backend->xkb_select_events_details.newKeyboardDetails =
-		backend->xkb_select_events_details.affectNewKeyboard;
-
-	backend->xkb_select_events_details.stateDetails =
-		backend->xkb_select_events_details.affectState;
-
-	// more of it
-	uint16_t events =
-		XCB_XKB_EVENT_TYPE_NEW_KEYBOARD_NOTIFY
-		| XCB_XKB_EVENT_TYPE_MAP_NOTIFY
-		| XCB_XKB_EVENT_TYPE_STATE_NOTIFY;
-
-	uint16_t map_parts = 
-		XCB_XKB_MAP_PART_KEY_TYPES
-		| XCB_XKB_MAP_PART_KEY_SYMS
-		| XCB_XKB_MAP_PART_MODIFIER_MAP
-		| XCB_XKB_MAP_PART_EXPLICIT_COMPONENTS
-		| XCB_XKB_MAP_PART_KEY_ACTIONS
-		| XCB_XKB_MAP_PART_VIRTUAL_MODS
-		| XCB_XKB_MAP_PART_VIRTUAL_MOD_MAP;
-
-	// classic xcb function with 321948571 parameters
-	xcb_void_cookie_t cookie =
-		xcb_xkb_select_events_aux_checked(
-			backend->conn,
-			backend->xkb_device_id,
-			events,
-			0,
-			0,
-			map_parts,
-			map_parts,
-			&(backend->xkb_select_events_details));
-
-	xcb_generic_error_t* error_xcb =
-		xcb_request_check(
-			backend->conn,
-			cookie);
-
-	if (error_xcb != NULL)
-	{
-		willis_error_throw(context, error, WILLIS_ERROR_WIN_XKB_SELECT_EVENTS);
-		return;
-	}
-
-	willis_error_ok(error);
-}
-
-void win_helpers_update_keymap(
-	struct willis* context,
-	struct willis_error_info* error)
-{
-	struct win_backend* backend = context->backend_data;
-	struct willis_xkb* xkb_common = backend->xkb_common;
-
-	struct xkb_keymap* keymap =
-		xkb_win_keymap_new_from_device(
-			xkb_common->context,
-			backend->conn,
-			backend->xkb_device_id,
-			XKB_KEYMAP_COMPILE_NO_FLAGS);
-
-	if (keymap == NULL)
-	{
-		willis_error_throw(context, error, WILLIS_ERROR_WIN_XKB_KEYMAP_NEW);
-		return;
-	}
-
-	struct xkb_state* state =
-		xkb_win_state_new_from_device(
-			keymap,
-			backend->conn,
-			backend->xkb_device_id);
-
-	if (state == NULL)
-	{
-		xkb_keymap_unref(keymap);
-		willis_error_throw(context, error, WILLIS_ERROR_WIN_XKB_STATE_NEW);
-		return;
-	}
-
-	xkb_state_unref(xkb_common->state);
-	xkb_keymap_unref(xkb_common->keymap);
-	xkb_common->keymap = keymap;
-	xkb_common->state = state;
-	willis_error_ok(error);
-}
-
-enum willis_event_code win_helpers_translate_button(
-	xcb_button_t button)
-{
-	switch (button)
-	{
-		case XCB_BUTTON_INDEX_1:
-		{
-			return WILLIS_MOUSE_CLICK_LEFT;
-		}
-		case XCB_BUTTON_INDEX_3:
-		{
-			return WILLIS_MOUSE_CLICK_RIGHT;
-		}
-		case XCB_BUTTON_INDEX_2:
-		{
-			return WILLIS_MOUSE_CLICK_MIDDLE;
-		}
-		case XCB_BUTTON_INDEX_4:
-		{
-			return WILLIS_MOUSE_WHEEL_UP;
-		}
-		case XCB_BUTTON_INDEX_5:
-		{
-			return WILLIS_MOUSE_WHEEL_DOWN;
-		}
-		default:
-		{
-			return WILLIS_NONE;
-		}
-	}
-}
-
-void win_helpers_handle_xkb(
-	struct willis* context,
-	xcb_generic_event_t* event,
-	struct willis_error_info* error)
-{
-	struct win_backend* backend = context->backend_data;
-	struct willis_xkb* xkb_common = backend->xkb_common;
-
-	// initialize error here to make the switch below more readable
-	willis_error_ok(error);
-
-	union willis_magic_xkb_event* xkb_event =
-		(union willis_magic_xkb_event*) event;
-
-	if (xkb_event->magic.device_id != backend->xkb_device_id)
-	{
-        return;
-	}
-
-	switch (xkb_event->magic.xkb_type)
-	{
-		case XCB_XKB_NEW_KEYBOARD_NOTIFY:
-		{
-			if ((xkb_event->keyboard.changed & XCB_XKB_NKN_DETAIL_KEYCODES) != 0)
-			{
-				win_helpers_update_keymap(
-					context,
-					error);
-			}
-
-			break;
-		}
-		case XCB_XKB_MAP_NOTIFY:
-		{
-			win_helpers_update_keymap(
-				context,
-				error);
-
-			break;
-		}
-		case XCB_XKB_STATE_NOTIFY:
-		{
-			xkb_state_update_mask(
-				xkb_common->state,
-				xkb_event->state.baseMods,
-				xkb_event->state.latchedMods,
-				xkb_event->state.lockedMods,
-				xkb_event->state.baseGroup,
-				xkb_event->state.latchedGroup,
-				xkb_event->state.lockedGroup);
-
-			break;
-		}
-		default:
-		{
-			break;
-		}
-	}
-
-	// error always set
+	return keycode_table_win[code];
 }
--- src/win/win_helpers.h
+++ src/win/win_helpers.h
@@ -4,25 +4,7 @@
 #include "include/willis.h"
 #include "win/win.h"
 
-void win_helpers_select_events_cursor(
-	struct willis* context,
-	uint32_t mask,
-	struct willis_error_info* error);
-
-void win_helpers_select_events_keyboard(
-	struct willis* context,
-	struct willis_error_info* error);
-
-void win_helpers_update_keymap(
-	struct willis* context,
-	struct willis_error_info* error);
-
-enum willis_event_code win_helpers_translate_button(
-	xcb_button_t button);
-
-void win_helpers_handle_xkb(
-	struct willis* context,
-	xcb_generic_event_t* event,
-	struct willis_error_info* error);
+enum willis_event_code win_helpers_keycode_table(
+	uint8_t code);
 
 #endif
